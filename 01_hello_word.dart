d195aa11b8838777ea99e96ca923ca5767468f8b
Jaime A Perez R <catecppcemp@gmail.com>
2023-03-30 16:30:53 -0500

    Dart start. Hello world and variables

---
+++ 01_hello_word.dart
@@ -0,0 +1,10 @@
+void main() {
+  
+  var myName = 'Ger';
+  String myNameString = 'Ger';
+  const myNameConst = 'Ger';
+
+
+  print('hola $myNameString');
+  print('hola ${ myNameConst.toUpperCase() }');
+}
