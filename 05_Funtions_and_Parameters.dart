18267abec9196bb88b67eae963ccb2b7938f9cb0
Jaime A Perez R <catecppcemp@gmail.com>
2023-04-05 16:03:39 -0500

    Parameters whit name

--- 05_Funtions_and_Parameters.dart
+++ 05_Funtions_and_Parameters.dart
@@ -1,5 +1,10 @@
 void main() {
   print(greetEveryone());
+  
+  print( 'Suma: ${ addTwoNumbersOptional(10 , 23)}');
+
+  print(greetPerson(name: 'Fernando' , message: 'Hi' ));
+
 }
 
 String greetEveryone() => 'Hello everyone!';
@@ -7,7 +12,12 @@ String greetEveryone() => 'Hello everyone!';
 int addTwoNumbers( int a, int b) => a + b;
 
 
-int addTwoNumbersOptional( int a, [int b = 0]) { /* Put the argument between [] to make this not necessary */
+int addTwoNumbersOptional( int a, [int b = 0]) { /* Surround the arguments with [] to make this argument optional adn by position */
   // b = b ?? 0; 
   return a + b;
 }
+
+
+String greetPerson({ required String name, String message = 'Hola,' }) { /* Surround the arguments with {} to make this argument optional and by name.) */
+  return '$message $name' ;
+} 
